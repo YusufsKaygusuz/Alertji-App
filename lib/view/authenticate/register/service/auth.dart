12aaee20cf0561bac1c56b3512a716b990a38aa4
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-05-11 01:12:49 +0300

    Update auth.dart

--- lib/view/authenticate/register/service/auth.dart
+++ lib/view/authenticate/register/service/auth.dart
@@ -3,9 +3,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 class Auth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
+  // Mevcut kullanıcıyı döndüren getter
   User? get currentUser => _firebaseAuth.currentUser;
+  
+  // Oturum durumu değişikliklerini dinleyen bir akış döndüren getter
   Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
 
+   // E-posta ve şifre ile giriş yapmayı sağlayan işlev
   Future<void> signInWithEmailAndPassword({
     required String email,
     required String password,
@@ -13,7 +17,7 @@ class Auth {
     await _firebaseAuth.signInWithEmailAndPassword(
         email: email, password: password);
   }
-
+  // E-posta ve şifre ile kullanıcı oluşturmayı sağlayan işlev
   Future<void> createUserWithEmailAndPassword({
     required String email,
     required String password,
@@ -21,7 +25,7 @@ class Auth {
     await _firebaseAuth.createUserWithEmailAndPassword(
         email: email, password: password);
   }
-
+   // Kullanıcıyı oturumdan çıkarmayı sağlayan işlev
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
   }
