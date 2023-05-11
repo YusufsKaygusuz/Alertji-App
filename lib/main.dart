e89e9f0a6735a480f955080f5c95e495627276fb
Yusuf S. Kaygusuz <yusufskaygusuz@gmail.com>
2023-05-11 17:27:29 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
