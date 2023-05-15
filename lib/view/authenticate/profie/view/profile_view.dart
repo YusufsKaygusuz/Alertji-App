b3e373bdf30ca4b35ea181667a8da6f60544d94f
EmirKosekul <88345933+EmirKosekul@users.noreply.github.com>
2023-05-15 18:08:33 +0300

    Some logout issues is solved.

--- lib/view/authenticate/profie/view/profile_view.dart
+++ lib/view/authenticate/profie/view/profile_view.dart
@@ -25,7 +25,7 @@ class _ProfilePageState extends State<ProfilePage> {
             ElevatedButton(
               onPressed: (){
                 Auth().signOut();
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => const LoginPage())); // const eklendi.
+                Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) => LoginPage())); // const eklendi.
               },
                child: const Text("Logout"))
           ],
