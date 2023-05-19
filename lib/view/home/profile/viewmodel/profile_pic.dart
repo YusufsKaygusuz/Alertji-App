ae980de8d6749d50c21f7b1ce455911143441df3
emiiryilmaz <emiryilmaz3346@gmail.com>
2023-05-19 16:30:32 +0300

    Added icons to profile page.

--- lib/view/home/profile/viewmodel/profile_pic.dart
+++ lib/view/home/profile/viewmodel/profile_pic.dart
@@ -1,9 +1,45 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ProfilePic extends StatelessWidget {
+class ProfilePic extends StatefulWidget {
+  final ValueChanged<File?> onImageChanged;
+  final File? initialImage;
+
   const ProfilePic({
     Key? key,
+    required this.onImageChanged,
+    this.initialImage,
   }) : super(key: key);
+
+  @override
+  _ProfilePicState createState() => _ProfilePicState();
+}
+
+class _ProfilePicState extends State<ProfilePic> {
+  late ImagePicker _imagePicker;
+  File? _imageFile;
+
+  @override
+  void initState() {
+    super.initState();
+    _imagePicker = ImagePicker();
+    _imageFile = widget.initialImage;
+  }
+
+  Future<void> _pickImage() async {
+    final pickedImage =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      final newImageFile = File(pickedImage.path);
+      widget.onImageChanged(newImageFile);
+
+      setState(() {
+        _imageFile = newImageFile;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +50,11 @@ class ProfilePic extends StatelessWidget {
         fit: StackFit.expand,
         clipBehavior: Clip.none,
         children: [
-          const CircleAvatar(
-            backgroundImage: AssetImage("assets/images/onboard2.jpg"),
+          CircleAvatar(
+            backgroundImage: _imageFile != null
+                ? FileImage(_imageFile!)
+                : AssetImage("assets/images/onboard2.jpg")
+                    as ImageProvider<Object>?,
           ),
           Positioned(
             right: -16,
@@ -32,7 +71,7 @@ class ProfilePic extends StatelessWidget {
                   ),
                   backgroundColor: const Color(0xFFF5F6F9),
                 ),
-                onPressed: () {},
+                onPressed: _pickImage,
                 child: const Icon(Icons.camera_alt),
               ),
             ),
