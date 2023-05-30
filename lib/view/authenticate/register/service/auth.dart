7164782d5a9cf66892c0528d36212a38eeb89499
Yusuf S. Kaygusuz <yusufskaygusuz@gmail.com>
2023-05-31 00:22:46 +0300

    debugging_process

--- lib/view/authenticate/register/service/auth.dart
+++ lib/view/authenticate/register/service/auth.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
 
 class Auth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
