3c8e4a9a1303fa3507739e18fc552cffcb6dde90
Yusuf S. Kaygusuz <yusufskaygusuz@gmail.com>
2023-06-05 00:15:01 +0300

    Update body.dart
    -> Clean Code tekniği uygulandı.
    -> Kod Tekrarı sorunu giderildi.

--- lib/view/home/profile/viewmodel/body.dart
+++ lib/view/home/profile/viewmodel/body.dart
@@ -6,8 +6,7 @@ import '../../../authenticate/login/service/auth.dart';
 import 'profile_menu.dart';
 
 class Body extends StatefulWidget {
-  // ignore: use_key_in_widget_constructors
-  const Body({Key? key});
+  const Body({Key? key}) : super(key: key);
 
   @override
   // ignore: library_private_types_in_public_api
@@ -15,6 +14,47 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  List<Map<String, dynamic>> get profileMenuItems => [
+    {
+      'text': 'Hesabım',
+      'iconData': Icons.person,
+      'route': () {},
+    },
+    {
+      'text': 'Bildirimler',
+      'iconData': Icons.notification_add,
+      'route': () {},
+    },
+    {
+      'text': 'Ayarlar',
+      'iconData': Icons.settings,
+      'route': () {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => const SettingsPage()),
+            (Route<dynamic> route) => false);
+      },
+    },
+    {
+      'text': 'Destek',
+      'iconData': Icons.help_center,
+      'route': () {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => const SupportPage()),
+            (Route<dynamic> route) => false);
+      },
+    },
+    {
+      'text': 'Çıkış Yap',
+      'iconData': Icons.logout,
+      'route': () {
+        Auth().signOut();
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => const LoginPage()),
+            (Route<dynamic> route) => false);
+      },
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -22,42 +62,17 @@ class _BodyState extends State<Body> {
       child: Column(
         children: [
           const SizedBox(height: 20),
-          ProfileMenu(
-            text: "Hesabım",
-            iconData: Icons.person,
-            press: () {},
-          ),
-          ProfileMenu(
-            text: "Bildirimler",
-            iconData: Icons.notification_add,
-            press: () {},
-          ),
-          ProfileMenu(
-            text: "Ayarlar",
-            iconData: Icons.settings,
-            press: () {
-              Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: (context) => const SettingsPage()),
-                  (Route<dynamic> route) => false);
-            },
-          ),
-          ProfileMenu(
-            text: "Destek",
-            iconData: Icons.help_center,
-            press: () {
-              Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: (context) => SupportPage()),
-                  (Route<dynamic> route) => false);
-            },
-          ),
-          ProfileMenu(
-            text: "Çıkış Yap",
-            iconData: Icons.logout,
-            press: () {
-              Auth().signOut();
-              Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: (context) => const LoginPage()),
-                  (Route<dynamic> route) => false);
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: profileMenuItems.length,
+            itemBuilder: (context, index) {
+              final menuItem = profileMenuItems[index];
+              return ProfileMenu(
+                text: menuItem['text'],
+                iconData: menuItem['iconData'],
+                press: menuItem['route'],
+              );
             },
           ),
         ],
