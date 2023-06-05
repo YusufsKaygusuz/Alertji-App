4341c248e636f23b33b61b623ad989184cbd1a22
Yusuf S. Kaygusuz <yusufskaygusuz@gmail.com>
2023-06-05 18:03:03 +0300

    Update login_view.dart

--- lib/view/authenticate/login/view/login_view.dart
+++ lib/view/authenticate/login/view/login_view.dart
@@ -231,7 +231,6 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                         ),
                         onTap: () {
-                          //Google auth kodları buraya yazılacak
                         },
                       ),
                     ],
